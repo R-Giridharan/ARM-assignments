3e78a4772c442014787946a4616f60073a322fd3
R-Giridharan <rgiridharan04@gmail.com>
2026-03-28 12:39:33 +0530

    Add files via upload

---
+++ 8led.c
@@ -0,0 +1,28 @@
+#include<lpc21xx.h>
+
+void delay(unsigned int n)
+{unsigned int i;
+for(;n>0;n--)
+for(i=12000;i>0;i--);
+}
+
+int main()
+{
+int i, count=0;
+PINSEL0=0;
+IODIR0=0xff;
+while(count<20)
+{
+IOCLR0=0xff;
+delay(500);
+IOSET0=0xff;
+delay(500);
+count++;
+}
+ for(i=0;i<8;i++)
+ {	IOCLR0=1<<i;
+delay(500);
+IOSET0=0xff;
+delay(500);
+ }
+}
