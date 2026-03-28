e58062ad6c25cac7ef2f4e6b6e9f1181827ad4d7
R-Giridharan <rgiridharan04@gmail.com>
2026-03-28 12:36:54 +0530

    Add files via upload

---
+++ BIT0-255.c
@@ -0,0 +1,27 @@
+ #include<lpc21xx.h>
+ void delay(unsigned int n)
+{unsigned int i;
+
+for(;n>0;n--)
+
+for(i=12000;i>0;i--);
+}
+ int main()
+ {
+ unsigned char i,count=0;
+ IODIR0=0XFF;
+ while(count<1)
+ {
+ for (i=0;i<256;i++)
+ {
+ IOCLR0=i;
+
+delay(500);
+
+IOSET0=0xff;
+
+delay(500);
+ }
+ }
+ count++;
+ }
